f908b85024c408ab6a62f16ab1b278ab07597c07
lubimo <sidorov.kirill198423@gmail.com>
2017-03-13 23:57:37 +0200

    Smarty connection

--- config/config.php
+++ config/config.php
@@ -1,5 +1,27 @@
 <?php
 
-//Константы для обращения к контроллерам
+//Constants for  calling to controllers
 define('PathPrefix', '../controllers/');
 define('PathPostfix', 'Controller.php');
+
+//Template used
+$template = 'default';
+
+//paths to files of templates(*.tpl)
+define('TemplatePrefix', "../views/($template)/");
+define('TemplatePostfix', '.tpl');
+
+//paths to files of templates in web space
+define('TemplateWebPath', "/templates/{$template}/");
+
+//init Smarty
+//put full path to Smarty.class.php
+require ('../library/Smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TemplatePrefix);
+$smarty->setCompileDir('../tmp/smarty/templates_c');
+$smarty->setCacheDir('../tmp/smarty/cache');
+$smarty->setConfigDir('../library/Smarty/configs');
+
+$smarty->assign('templateWebPath', TemplateWebPath);
